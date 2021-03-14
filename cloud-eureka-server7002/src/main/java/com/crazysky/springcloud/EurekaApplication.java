b626baa0ed13470af40f799215c711791525eb7a
Crazy-Sky <1426627870@qq.com>
2021-03-14 17:02:25 +0800

    添加子模块cloud-eureka-server7002

---
+++ cloud-eureka-server7002/src/main/java/com/crazysky/springcloud/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.crazysky.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka 7002注册中心服务启动类
+ * @author CrazySky
+ * @date 2021/03/14 15:14
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
